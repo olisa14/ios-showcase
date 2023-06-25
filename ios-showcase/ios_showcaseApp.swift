1dfa14a0220d96e4300757645fb09d4cf6037f2f
olisa <olisaobinna@gmail.com>
2023-06-25 12:09:50 -0500

    initial commit

---
+++ ios-showcase/ios_showcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_showcaseApp.swift
+//  ios-showcase
+//
+//  Created by olisa okpoko on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_showcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
